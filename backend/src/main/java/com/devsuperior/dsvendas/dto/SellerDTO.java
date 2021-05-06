20190ca46efc8faa1d6ceaa3fd6761b56ce10fac
Alanis2 <alanisemanuela950@gmail.com>
2021-05-06 03:19:50 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
@@ -0,0 +1,40 @@
+package com.devsuperior.dsvendas.dto;
+
+import com.devsuperior.dsvendas.entities.Seller;
+
+import java.io.Serializable;
+
+public class SellerDTO implements Serializable {
+
+    private Long id;
+    private String name;
+
+    public SellerDTO(){
+    }
+
+    public SellerDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public SellerDTO(Seller entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
