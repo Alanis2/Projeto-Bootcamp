63e7b266a0708432ec6189b6849069c24eddb163
Alanis2 <alanisemanuela950@gmail.com>
2021-05-07 01:33:30 -0300

    Pagination

---
+++ backend/src/main/java/com/devsuperior/dsvendas/repository/SaleRepository.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dsvendas.repository;
+
+import com.devsuperior.dsvendas.entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
